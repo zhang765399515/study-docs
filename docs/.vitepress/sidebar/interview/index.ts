0eee0df658da0869507994e58597aeb9dccfdb12
project <32183131+zhang765399515@users.noreply.github.com>
2025-02-06 10:23:04 +0800

    更新两个案例

--- docs/.vitepress/sidebar/interview/index.ts
+++ docs/.vitepress/sidebar/interview/index.ts
@@ -3,10 +3,18 @@ export default [
         text: "算法系列",
         collapsed: false,
         items: [
-                    {
-                        text: "数字类型度限制",
-                        link: "/markdown/interview/algorithm/numberLength",
-                    }
+            {
+                text: "数字类型度限制",
+                link: "/markdown/interview/algorithm/numberLength",
+            },
+            {
+                text: "爬楼梯",
+                link: "/markdown/interview/algorithm/ClimbStairs",
+            },
+            {
+                text: "买股票最大收益",
+                link: "/markdown/interview/algorithm/StockEarnings",
+            },
         ],
     },
 ];
